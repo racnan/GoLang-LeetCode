630a047ded07fc73ff65eb6473b075675df5a2ab
Rachit <rachitdev45@gmail.com>
2021-08-02 13:06:21 +0530

    Linked List #5

---
+++ LinkedList/ReverseLinkedList.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	curr := head
+	var prev, next *ListNode
+
+	for curr != nil {
+		next = curr.Next
+		curr.Next = prev
+
+		prev = curr
+		curr = next
+	}
+
+	return prev
+
+}
